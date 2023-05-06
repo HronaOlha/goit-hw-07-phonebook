8acf11112fbc53cec51d0aa2020cfb517511221b
Грона Оля <grona.hell@gmail.com>
2023-05-06 14:02:32 +0300

    fix a little bit

--- src/redux/reducerFetch.js
+++ src/redux/reducerFetch.js
@@ -1,15 +1,31 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const fetchMovies = async () => {
-  // const data = await fetch(
-  //   `https://api.themoviedb.org/3/trending/movie/day?api_key=2ead4d55a2c7da4f5313610b563685be`
-  // );
-  // const movies = await data.json();
+  const data = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=2ead4d55a2c7da4f5313610b563685be`
+  );
+  const movies = await data.json();
+  return movies;
 };
 
 export const movieFetch = createAsyncThunk('movies/getTopMovies', async () => {
   return await fetchMovies();
 });
+
+const handlePending = (state, action) => {
+  state.isLoading = true;
+};
+
+const handleFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.movies = action.payload;
+  state.error = '';
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 const initialState = {
   movies: null,
@@ -20,23 +36,30 @@ const initialState = {
 const mySlice = createSlice({
   name: 'movies',
   initialState,
-  extraReducers: {
-    [movieFetch.pending]: (state, action) => {
-      state.isLoading = true;
-    },
-    [movieFetch.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.movies = action.payload;
-      state.error = '';
-    },
-    [movieFetch.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(movieFetch.pending, handlePending)
+      .addCase(movieFetch.fulfilled, handleFulfilled)
+      .addCase(movieFetch.rejected, handleRejected);
   },
 });
 
 export const moviesReducer = mySlice.reducer;
+
+// extraReducers: {
+//   [movieFetch.pending]: (state, action) => {
+//     state.isLoading = true;
+//   },
+//   [movieFetch.fulfilled]: (state, action) => {
+//     state.isLoading = false;
+//     state.movies = action.payload;
+//     state.error = '';
+//   },
+//   [movieFetch.rejected]: (state, action) => {
+//     state.isLoading = false;
+//     state.error = action.payload;
+//   },
+// },
 
 // export const movieFetch = () => {
 //   return async dispatch => {
